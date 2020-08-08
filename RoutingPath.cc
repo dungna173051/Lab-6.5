d2a73ea6431494296f667d0f32adb4e65c0131e3
dungna173051 <nad.ln.mine@gmail.com>
2020-08-08 11:19:57 +0700

    first commit

---
+++ RoutingPath.cc
@@ -0,0 +1,13 @@
+/*
+ * RoutingPath.cc
+ *
+ *  Created on: Apr 25, 2020
+ *      Author: hongt
+ */
+
+#include "RoutingPath.h"
+
+RoutingPath::RoutingPath(){
+
+}
+
---
+++ RoutingPath.h
@@ -0,0 +1,19 @@
+/*
+ * RoutingPath.h
+ *
+ *  Created on: Apr 25, 2020
+ *      Author: hongt
+ */
+
+#ifndef ROUTINGPATH_H_
+#define ROUTINGPATH_H_
+#include <list>
+using namespace std;
+class RoutingPath
+{
+public:
+    list<int> path;
+    RoutingPath();
+};
+
+#endif /* ROUTINGPATH_H_ */
